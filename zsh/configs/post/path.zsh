74ddc7d4cf3b5735cf0eecdc6f7d0dd7f6fbda51
Aaron Mc Adam <aaron@aaronmcadam.com>
2018-05-08 12:07:02 +0100

    checkin

--- zsh/configs/post/path.zsh
+++ zsh/configs/post/path.zsh
@@ -1,11 +1,11 @@
-# ensure dotfiles bin directory is loaded first
-PATH="$HOME/.bin:/usr/local/sbin:$PATH"
-
 . $HOME/.asdf/asdf.sh
 . $HOME/.asdf/completions/asdf.bash
 
+# ensure dotfiles bin directory is loaded first
+PATH="$HOME/.bin:/usr/local/sbin:$PATH"
+
 # mkdir .git/safe in the root of repositories you trust
-PATH=".git/safe/../../bin:$PATH"
+PATH=".git/safe/../../bin:$HOME/.asdf/shims:$HOME/.asdf/bin:$PATH"
 
 . /usr/local/etc/profile.d/z.sh
 
